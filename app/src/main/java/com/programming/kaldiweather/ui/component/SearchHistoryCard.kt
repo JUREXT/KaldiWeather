1a2de177c3e33d5f1ab07f11cd68272c8af06dac
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-01-24 00:11:57 +0100

    Created SearchHistoryCard composable to show serach history.

---
+++ app/src/main/java/com/programming/kaldiweather/ui/component/SearchHistoryCard.kt
@@ -0,0 +1,52 @@
+package com.programming.kaldiweather.ui.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.programming.kaldiweather.ui.theme.KaldiWeatherTheme
+
+@Composable
+fun SearchHistoryCard(
+    modifier: Modifier = Modifier,
+    element: String
+) {
+    Card(
+        modifier = modifier
+            .padding(5.dp)
+            .shadow(8.dp, RoundedCornerShape(8.dp)),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(8.dp),
+        border = BorderStroke(width = 2.dp, color = Color.DarkGray)
+    ) {
+        Text(
+            text = element,
+            modifier = Modifier
+                .wrapContentHeight()
+                .padding(5.dp),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchHistoryCardPreview() {
+    KaldiWeatherTheme {
+        SearchHistoryCard(
+            element = "Element"
+        )
+    }
+}
