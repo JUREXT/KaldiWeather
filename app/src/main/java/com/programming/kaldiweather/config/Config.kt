917bf3d10a836254ccffcdca03819d270afb9a54
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-01-26 22:15:53 +0100

    Defined API endpoints with additional config params to be used in api call.

---
+++ app/src/main/java/com/programming/kaldiweather/config/Config.kt
@@ -0,0 +1,12 @@
+package com.programming.kaldiweather.config
+
+object Config {
+    const val BASE_URL = "https://geocoding-api.open-meteo.com"
+    private const val API_VERSION_1 = BASE_URL.plus("/v1")
+
+    const val API_V1_SEARCH_CITY = API_VERSION_1.plus("/search")
+
+    const val API_RESPONSE_COUNT = 1
+    const val API_RESPONSE_LANGUAGE = "en"
+    const val API_RESPONSE_FORMAT_JSON = "json"
+}
