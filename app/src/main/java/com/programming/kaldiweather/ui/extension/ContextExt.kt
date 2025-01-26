42ad0404cf1f6730e7912b9d6b8f2556a0c74361
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-01-26 20:50:47 +0100

    Created extension fun isConnectedToInternet on Context to check for connection.

--- app/src/main/java/com/programming/kaldiweather/ui/extension/ContextExt.kt
+++ app/src/main/java/com/programming/kaldiweather/ui/extension/ContextExt.kt
@@ -1,8 +1,12 @@
 package com.programming.kaldiweather.ui.extension
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.core.content.getSystemService
 
 fun Context.findActivity(): Activity {
     var context = this
@@ -11,4 +15,15 @@ fun Context.findActivity(): Activity {
         context = context.baseContext
     }
     throw IllegalStateException("No Activity found.")
+}
+
+@SuppressLint("MissingPermission")
+fun Context.isConnectedToInternet(): Boolean {
+    return getSystemService<ConnectivityManager>()?.run {
+        activeNetwork ?: return false
+        getNetworkCapabilities(activeNetwork)?.run {
+            hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    && hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        } ?: false
+    } ?: false
 }
