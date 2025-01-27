0129ba72e963e55ba87ac30948816d20ef906803
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-01-27 21:36:36 +0100

    Added weather forecast api end point.

--- app/src/main/java/com/programming/kaldiweather/config/Config.kt
+++ app/src/main/java/com/programming/kaldiweather/config/Config.kt
@@ -5,6 +5,7 @@ object Config {
     private const val API_VERSION_1 = BASE_URL.plus("/v1")
 
     const val API_V1_SEARCH_CITY = API_VERSION_1.plus("/search")
+    const val API_V1_WEATHER_FORECAST = "https://api.open-meteo.com/v1/forecast"
 
     const val API_RESPONSE_COUNT = 1
     const val API_RESPONSE_LANGUAGE = "en"
